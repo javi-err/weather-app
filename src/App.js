c20641aa90091817520efa25971bc8b08de7cb31
javi <jsmart1993@hotmail.com>
2020-10-14 13:51:27 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Weather from './Components/Weather'
+import './index.css';
+import 'tachyons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+const apikey = "638e10fa0355fe08af8c6d4076f8da62"
+
+export class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      city: undefined,
+      country: undefined
+    };
+   console.log(this.state)
+  }
+
+
+  componentDidMount() {
+    this.getWeather();
+
+
+  }
+
+  componentDidUpdate() {
+    if(this.state.country == 'US') {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=Denver,US&appid=${apikey}`)
+      .then(response => response.json())
+      .then(data => this.setState({
+        city: data.name,
+        country: data.sys.country
+        
+      }));
+    }
+  }
+//handle functions
+
+  getWeather = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=${apikey}`)
+  .then(response => response.json())
+  .then(data => this.setState({
+    city: data.name,
+    country: data.sys.country
+    
+  }));
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+      <Weather city={this.state.city} country={this.state.country}/>
+  </div>
+    )
+
+}
+
 }
 
 export default App;
---
+++ src/Components/Weather.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+ const Weather = (props) => {
+    return (
+        <div>
+            <div className="container pa5">
+                <div className="weather-card flex flex-column tc">
+                    <div className="weather-title">
+                        <h1>{props.city}, {props.country}</h1>
+                        <h5>Clear</h5>
+                    </div>
+                    <div className="temperature-container">
+                        <div className="temperature">
+                            <h1 className="degrees">52&deg;</h1>
+                            <span className="high pr2">H: 82&deg;</span>
+                            <span className="low">L: 22&deg;</span>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+export default Weather;
